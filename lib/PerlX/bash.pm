b0f38ec52242a922812b8980e7a60d6710684f5b
Buddy Burden <barefootcoder@gmail.com>
2016-06-14 20:41:18 -0700

    absolute most basic functionality implemented
    `bash` function
    takes a string, runs it as a bash command
    properly handles simple errors
    returns true/false in boolean context or exit value in scalar context

--- lib/PerlX/bash.pm
+++ lib/PerlX/bash.pm
@@ -3,7 +3,24 @@ package PerlX::bash;
 use strict;
 use warnings;
 
+use Exporter 'import';
+our @EXPORT = ('bash');
+
 # VERSION
+
+
+use Contextual::Return;
+use IPC::System::Simple qw< run capture EXIT_ANY $EXITVAL >;
+
+
+sub bash
+{
+	run [0..125], qw< bash -c >, shift;
+	return
+		BOOL	{	$EXITVAL == 0	}
+		SCALAR	{	$EXITVAL		}
+	;
+}
 
 
 1;
